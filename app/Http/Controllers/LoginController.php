cfd42e7892c4e4c934b2960904856e172bc9d571
Carlos Ramirez <cramirezballesteros5@gmail.com>
2021-02-25 10:29:30 -0500

    Creando MVC para las tablas

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Login;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class LoginController extends Controller
+{
+
+    public function createLogin(Request $request)
+    {
+        $Login = new Login;
+        $Login->correo = $request->correo;
+        $Login->password = $request->password;
+        $Login->save();
+        echo "Inicio de sesión exitoso";
+        return response()->json($Login)->setStatusCode(Response::HTTP_OK);
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Login  $login
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Login $login)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Login  $login
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Login $login)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Login  $login
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Login $login)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Login  $login
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Login $login)
+    {
+        //
+    }
+}
